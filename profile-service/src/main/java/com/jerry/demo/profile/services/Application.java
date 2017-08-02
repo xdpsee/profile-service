4de56c59038d227f0178a35dd926cafe2eac7f3a
zhenhui.czh <zhenhui.czh@alibaba-inc.com>
2017-08-02 20:41:51 +0800

    appassembler

---
+++ profile-service/src/main/java/com/jerry/demo/profile/services/Application.java
@@ -0,0 +1,11 @@
+package com.jerry.demo.profile.services;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello,profile!");
+
+    }
+
+}
